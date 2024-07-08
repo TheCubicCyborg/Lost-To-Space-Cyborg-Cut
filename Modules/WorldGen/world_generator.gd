220214f0518a5d9f41b5982cc7a1fd1deb30c54f
TheCubicCyborg <alexg555.ag@gmail.com>
2024-07-08 13:35:21 -0700

    Messing with perlin noise

--- Modules/WorldGen/world_generator.gd
+++ Modules/WorldGen/world_generator.gd
@@ -1,11 +1,18 @@
 extends Node
 
+var surface = FastNoiseLite.new()
+@onready var tilemap = $TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	surface.seed = randi()
+	surface.noise_type = FastNoiseLite.TYPE_PERLIN
+	for x in range(0,100):
+		tilemap.set_cell(0,Vector2i(x,10+int(surface.get_noise_1d(x)*5)),0,Vector2i(0,0))
+		#tilemap.set_cell(0,Vector2i(x,10),0,Vector2i(0,0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
